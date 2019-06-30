e65f0b4d87c52181f46c050ab150e3f7b7574b6f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-30 09:50:42 +0000

    Deploy SebastianSchmidt/breakout to github.com/SebastianSchmidt/breakout.git:gh-pages

--- src/App.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from '@vue/test-utils'
-import App from '@/App.vue'
-
-describe('App.vue', () => {
-  it('renders message', () => {
-    const wrapper = shallowMount(App)
-    expect(wrapper.text()).toMatch('Breakout')
-  })
-})
